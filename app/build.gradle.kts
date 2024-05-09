7dd9acc0fe0dc80fa7f9802acefba713786e6c64
hemraj.axestrack <hemraj.kumawat@axestrack>
2024-05-09 14:42:00 +0530

    minimum sdk change

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.hemu1089.uhf"
-        minSdk = 23
+        minSdk = 19
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
